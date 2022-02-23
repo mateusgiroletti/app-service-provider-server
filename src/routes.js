e07586d96e84cda215744d8eba87e193ecc6cd72
Mateus Giroletti <mateusgiroletti97@gmail.com>
2022-02-22 21:41:41 -0300

    Adding routes for auth and users

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
 
+import UserController from "./app/controllers/UserController";
+import SessionController from "./app/controllers/SessionController";
+
+import authMiddleware from "./app/middlewares/auth";
+
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-    res.json({ msg: "ok" });
-});
+routes.post("/users", UserController.store);
+routes.post("/sessions", SessionController.store);
+
+routes.use(authMiddleware);
+
+// From here down -  Routes that require authentication
 
 export default routes;
