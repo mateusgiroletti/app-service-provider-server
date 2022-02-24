b68a7edb80c05d6a08187f66e4729af4646611c6
Mateus Giroletti <mateusgiroletti97@gmail.com>
2022-02-23 22:28:27 -0300

    Adding news routes

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,39 @@
 import { Router } from "express";
+import multer from "multer";
+import multerConfig from "./config/multer";
 
 import UserController from "./app/controllers/UserController";
 import SessionController from "./app/controllers/SessionController";
+import FileController from "./app/controllers/FileController";
+import ProvidersController from "./app/controllers/ProvidersController";
+import AppointmentController from "./app/controllers/AppointmentController";
+import ScheduleController from "./app/controllers/ScheduleController";
+import NotificationController from "./app/controllers/NotificationController";
+import AvaliableController from "./app/controllers/AvaliableController";
 
 import authMiddleware from "./app/middlewares/auth";
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post("/users", UserController.store);
 routes.post("/sessions", SessionController.store);
 
 routes.use(authMiddleware);
 
-// From here down -  Routes that require authentication
+routes.put("/users", UserController.update);
 
+routes.get("/appointments", AppointmentController.index);
+routes.post("/appointments", AppointmentController.store);
+routes.delete("/appointments/:id", AppointmentController.delete);
+
+routes.get("/schedule", ScheduleController.index);
+
+routes.get("/providers", ProvidersController.index);
+routes.get("/providers/:providerId/available", AvaliableController.index);
+
+routes.get("/notifications", NotificationController.index);
+routes.put("/notifications/:id", NotificationController.update);
+
+routes.post("/files", upload.single("file"), FileController.store);
 export default routes;
