7c559b1e02457ceb85b42eac334ca29e620cdbc4
Mateus Giroletti <mateusgiroletti97@gmail.com>
2022-02-25 22:42:39 -0300

    Adjusting auth

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -15,7 +15,7 @@ export default async (req, res, next) => {
     try {
         const decoded = await promisify(jwt.verify)(token, authConfig.secret);
 
-        req.userId = decoded.id;
+        req.userId = decoded.id || null;
 
         return next();
     } catch (error) {
