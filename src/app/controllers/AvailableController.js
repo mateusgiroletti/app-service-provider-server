49101bafd7cd5a1a0df76152d34f1bc5e1c7ff10
Mateus Giroletti <mateusgiroletti97@gmail.com>
2022-08-21 12:20:02 -0300

    Abstraindo lógica do available controller

--- src/app/controllers/AvailableController.js
+++ src/app/controllers/AvailableController.js
@@ -1,15 +1,5 @@
-import {
-    parseISO,
-    startOfDay,
-    endOfDay,
-    setHours,
-    setMinutes,
-    setSeconds,
-    format,
-    isAfter,
-} from "date-fns";
-import { Op } from "sequelize";
-import Appointment from "../models/Appointment";
+import { parseISO, } from "date-fns";
+import AvailableService from "../services/AvailableService";
 
 class AvailableController {
     async index(req, res) {
@@ -22,48 +12,8 @@ class AvailableController {
 
         const searchDate = parseISO(date);
 
-        const appointments = await Appointment.findAll({
-            where: {
-                provider_id: providerId,
-                canceled_at: null,
-                date: {
-                    [Op.between]: [
-                        startOfDay(searchDate),
-                        endOfDay(searchDate),
-                    ],
-                },
-            },
-        });
-
-        const schedule = [
-            "08:00",
-            "09:00",
-            "10:00",
-            "11:00",
-            "12:00",
-            "13:00",
-            "14:00",
-            "15:00",
-            "16:00",
-            "17:00",
-            "18:00",
-            "19:00",
-        ];
-
-        const available = schedule.map((time) => {
-            const [hour, minute] = time.split(":");
-            const value = setSeconds(
-                setMinutes(setHours(searchDate, hour), minute),
-                0
-            );
-
-            return {
-                time,
-                value: format(value, "yyyy-MM-dd'T'HH:mm:ssxxx"),
-                available:
-                    isAfter(value, new Date()) &&
-                    !appointments.find((a) => format(a.date, "HH:mm") === time),
-            };
+        const available = await AvailableService({
+            providerId, searchDate
         });
 
         return res.json(available);
